b88b37c694636a226af347b39e0287ba68df1ce8
EC2 Default User <ec2-user@ip-172-31-87-180.ec2.internal>
2023-12-20 07:31:12 +0000

    [Add]

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -3,7 +3,7 @@ class Public::CartItemsController < ApplicationController
   
   def index
     @cart_items = current_customer.cart_items
-    @store = Store.find(params[:store_id])
+    # @store = Store.find(params[:store_id])
     @total = 0
   end
   
@@ -13,8 +13,8 @@ class Public::CartItemsController < ApplicationController
     @cart_item.customer_id = current_customer.id
     if CartItem.find_by(item_id: params[:cart_item][:item_id])
       cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
-      cart_item.amount += params[:cart_item][:amount].to_i
-      cart_item.update(amount: cart_item.amount)
+      cart_item.quantity += params[:cart_item][:quantity].to_i
+      cart_item.update(quantity: cart_item.quantity)
       redirect_to cart_items_path
     else
       @cart_item = CartItem.new(cart_item_params)
@@ -47,10 +47,6 @@ class Public::CartItemsController < ApplicationController
   private
   
   def cart_item_params
-    params.require(:cart_item).permit(:item_id, :amount)
-  end
-  
-  def subtotal
-  item.with_tax_price * amount
+    params.require(:cart_item).permit(:item_id, :quantity, :store_id)
   end
 end
