358fbcb6d51036682db330bf0d9c9db595a3bf15
EC2 Default User <ec2-user@ip-172-31-87-180.ec2.internal>
2023-12-30 06:48:59 +0000

    [Add]

---
+++ db/migrate/20231230064644_add_number_to_orders.rb
@@ -0,0 +1,5 @@
+class AddNumberToOrders < ActiveRecord::Migration[6.1]
+  def change
+    add_column :orders, :number, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_14_122906) do
+ActiveRecord::Schema.define(version: 2023_12_30_064644) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -115,6 +115,7 @@ ActiveRecord::Schema.define(version: 2023_12_14_122906) do
     t.integer "payment_amount", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "number"
   end
 
   create_table "stores", force: :cascade do |t|
